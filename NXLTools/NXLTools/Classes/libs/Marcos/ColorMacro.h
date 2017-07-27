f513992bd8417a07c12a0048ce3001d0bb025a71
nanxinlin <18392056542@163.com>
2017-07-27 11:38:11 +0800

    11111111

--- NXLTools/NXLTools/Classes/libs/Marcos/ColorMacro.h
+++ NXLTools/NXLTools/Classes/libs/Marcos/ColorMacro.h
@@ -10,15 +10,11 @@
 #define ColorMacro_h
 
 // 颜色
-#define NXLColor(r, g, b)        [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:1.0]
+#define NXLColor(r, g, b)  [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:1.0]
+// 颜色呆透明度
 #define NXLColorRGBA(r, g, b, a) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:a]
 // 随机色
 #define NXLRandomColor NXLColor(arc4random_uniform(256), arc4random_uniform(256), arc4random_uniform(256))
-// 16进制字符串获取颜色
-#ifndef UIColorHex
-#define UIColorHex(_hex_)   [UIColor colorWithHexString:((__bridge NSString *)CFSTR(#_hex_))]
-#endif
-
 
 
 #endif /* ColorMacro_h */
